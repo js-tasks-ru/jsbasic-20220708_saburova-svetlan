7ce10d1dcdeea052c40f3df533b543de800c346c
Svetlana <saburova_svetlan@mail.ru>
2022-07-18 23:11:36 +0700

    Поиск факториала через цикл for

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,32 @@
+"use strict";
+
 function factorial(n) {
-  // ваш код...
+
+  let result = 1;
+
+  if (n < 0 ) {
+    return;
+  }
+  if (n === "1") {
+    return result;
+  }
+  for (let i = 1; i <= n; i++) { 
+      result = result * i;
+  }
+  return result;
 }
+
+let n = prompt("Факториал какого числа требуется вычислить?", '');
+
+if (n === "1") {
+  alert("1");
+
+} else if (n < 0 ){
+  alert("Ошибка");
+
+} else {
+  alert(factorial(n));
+}
+
+
+
