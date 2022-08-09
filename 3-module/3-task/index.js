e3f1dbbc9c844ab9d0d47e75836d233f2619bc5c
Svetlana <saburova_svetlan@mail.ru>
2022-08-09 11:32:38 +0700

    Преобразование текста в другой формат

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+
+  for (let i = 1; i <arr.length; i++) {
+    if (arr.length > 1) {
+      arr[i] = arr[i][0].toUpperCase()+ arr[i].slice(1);
+    } else {
+      return arr[i];
+    }
+  }
+
+  str = arr.join('');
+  return str;
 }
